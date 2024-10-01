484a49538ee59c4aa07863f9618b4fec02057804
Lucas Sinica <64593019+LukaszSinica@users.noreply.github.com>
2024-10-01 19:31:58 +0200

    security
    adding filterChain
    adding users to the db
    encoding password with bcrypt
    setting datasource to docker mysql db

--- workplace/src/main/java/com/lukaszsinica/workplace/WorkplaceApplication.java
+++ workplace/src/main/java/com/lukaszsinica/workplace/WorkplaceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.lukaszsinica.test")
 public class WorkplaceApplication {
 
 	public static void main(String[] args) {
