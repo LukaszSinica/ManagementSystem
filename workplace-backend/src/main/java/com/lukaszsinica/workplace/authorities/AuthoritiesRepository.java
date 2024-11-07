333476c0a4629201badf646f6d367a7461dafde3
Lucas Sinica <64593019+LukaszSinica@users.noreply.github.com>
2024-11-07 18:01:16 +0100

    update

--- workplace-backend/src/main/java/com/lukaszsinica/workplace/authorities/AuthoritiesRepository.java
+++ workplace-backend/src/main/java/com/lukaszsinica/workplace/authorities/AuthoritiesRepository.java
@@ -2,6 +2,6 @@ package com.lukaszsinica.workplace.authorities;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface AuthoritiesRepository extends CrudRepository<Authorities, String> {
+public interface AuthoritiesRepository extends CrudRepository<Authorities, Long> {
 
 }
