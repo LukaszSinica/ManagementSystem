ae8f7c065ecc13b2193ebc8e5f55bd4677d33993
Lucas Sinica <64593019+LukaszSinica@users.noreply.github.com>
2024-11-25 18:35:55 +0100

    trying tests in mockito and junit

--- workplace-backend/src/test/java/com/lukaszsinica/workplace/WorkplaceApplicationTests.java
+++ workplace-backend/src/test/java/com/lukaszsinica/workplace/WorkplaceApplicationTests.java
@@ -1,7 +1,15 @@
 package com.lukaszsinica.workplace;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.lukaszsinica.workplace.timer.TimerService;
 
 @SpringBootTest
 class WorkplaceApplicationTests {
@@ -9,5 +17,27 @@ class WorkplaceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	void calculateDifferenceInSecondsTest() {
+        TimerService timerService = new TimerService();
+        Timestamp fromTimestamp = Timestamp.valueOf(LocalDateTime.now());
+        Timestamp toTimestamp = Timestamp.valueOf(LocalDateTime.now().plusDays(2));
+        long expectedDifference = 2 * 24 * 60 * 60;
+		long timeDiffrence = timerService.calculateDifferenceInSeconds(fromTimestamp, toTimestamp);
+        assertEquals(expectedDifference, timeDiffrence,  "The difference in seconds is incorrect");
+	}
+	
+	@Test
+	void calculateDifferenceInSecondsMockitoTest() {
+	    TimerService timerService = mock(TimerService.class);
+	    Timestamp fromTimestamp = Timestamp.valueOf(LocalDateTime.now());
+	    Timestamp toTimestamp = Timestamp.valueOf(LocalDateTime.now().plusDays(2));
 
+	    when(timerService.calculateDifferenceInSeconds(fromTimestamp, toTimestamp))
+	        .thenReturn(172800L);
+
+	    long actualDifference = timerService.calculateDifferenceInSeconds(fromTimestamp, toTimestamp);
+	    assertEquals(1728000L, actualDifference, "The difference in seconds is incorrect");
+	}
 }
